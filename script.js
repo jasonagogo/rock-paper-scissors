9685a23eb948fd88bb619390f17396f80bad90b6
jasonagogo <jasonagogo@gmail.com>
2023-04-03 13:53:45 -0400

    add logic, isn't connected to anything yet.

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const player = document.querySelector('.player');
 const computer = document.querySelector('.computer');
 const pchoices = document.querySelectorAll('.playerSelection');
 state.textContent= "MAKE YOUR SELECTION!!!"
-const playerSelection= getPlayerChoice();
-const computerSelection= getComputerChoice();
+let playerSelection= getPlayerChoice();
+let computerSelection= getComputerChoice();
 
 //collect player selection through an onClick event
 function getPlayerChoice() {
@@ -30,23 +30,80 @@ function getComputerChoice() {
     const computerSelection= computer.addEventListener('click', 
       function handleClick(e) {
         const computerSelection = choices[Math.floor(Math.random()*choices.length)];
-        return computer.textContent = computerSelection;
+        computer.textContent = computerSelection;
+        document.getElementsByClassName('computer').textContent
       })
-}
+    }
 
 //compare playerSelection to computerSelection and determine winner
 
-function playRound() {
-  for (let i=0; i<5; i++) {
+
+for (let i=0; i<5; i++) {
     let winCount= 0;
     let lossCount= 0;
     let gameCount= i+1;
     state.textContent= "MAKE YOUR SELECTION!!!";
     playerSelection = getPlayerChoice();
     computerSelection = getComputerChoice();
+    let playerState = playerSelection.toUpperCase;
+    let computerState = computerSelection.toUpperCase;
+    if (playerState === computerState) {
+        computer.textContent = '';
+        player.removeAttribute('id');
+        state.textContent = tie + '     Game Count: ' + gameCount 
+        '     Wins: ' + winCount + '     Losses: ' + lossCount;
+        return
+    } else if (playerState === 'ROCK') {
+        if (computerState === 'PAPER') {
+          lossCount++
+          computer.textContent = '';
+          player.removeAttribute('id');
+          state.textContent = lose + '     Game Count: ' + gameCount 
+          '     Wins: ' + winCount + '     Losses: ' + lossCount;
+          return
+          } else {
+            winCount++
+            computer.textContent = '';
+            player.removeAttribute('id');
+            state.textContent = win + '     Game Count: ' + gameCount 
+            '     Wins: ' + winCount + '     Losses: ' + lossCount;
+            return
+          }
+        }
+      else if (playerState === 'PAPER') {
+        if (computerState === 'SCISSORS') {
+            lossCount++
+            computer.textContent = '';
+            player.removeAttribute('id');
+            state.textContent = lose + '     Game Count: ' + gameCount 
+            '     Wins: ' + winCount + '     Losses: ' + lossCount;
+            return
+            } else {
+              winCount++
+              computer.textContent = '';
+              player.removeAttribute('id');
+              state.textContent = win + '     Game Count: ' + gameCount 
+              '     Wins: ' + winCount + '     Losses: ' + lossCount;
+              return
+            }
+      }
+      else {
+        if (computerState === 'ROCK') {
+            lossCount++
+            computer.textContent = '';
+            player.removeAttribute('id');
+            state.textContent = lose + '     Game Count: ' + gameCount 
+            '     Wins: ' + winCount + '     Losses: ' + lossCount;
+            return
+            } else {
+              winCount++
+              computer.textContent = '';
+              player.removeAttribute('id');
+              state.textContent = win + '     Game Count: ' + gameCount 
+              '     Wins: ' + winCount + '     Losses: ' + lossCount;
+              return
+            }
+      }  
+
     //update gameCount, lossCount, winCount and return all three in state div
-  } 
-
-}
-
-
+  }
